f1cb67960ca7408cc9db9e01dee5d5235bdaa8b3
gaurav gahlot <udityagahlot@gmail.com>
2020-06-02 09:09:51 +0530

    shopping cart

--- Shopping cart/cart.java
+++ Shopping cart/cart.java
@@ -1,14 +1,11 @@
 
 import java.util.*;
-class Shopping {  
+class Shopping { 
 	int total=0,t=0;
-	int j,i;               // loop to print value.
-	ArrayList<Integer> a = new ArrayList<Integer>();   //Product quantity
-	ArrayList<Integer> b = new ArrayList<Integer>();   //Product ID
-	ArrayList<String> c = new ArrayList<String>();   //Product Name
 	public int addproduct, addquantity;
 	public int id[] = new int[5];
-
+	                                          ArrayList<Prod> productlist=new ArrayList<Prod>();
+	                                          ArrayList<Integer> quantity=new ArrayList<Integer>();
 	public void addProduct() {
            int id[] = new int[5];
 			id[0] = 1;
@@ -16,78 +13,87 @@ class Shopping {
 			id[2] = 3;
 			id[3] = 4;
 			id[4] = 5;
-			            String arr[] = new String[5];
-			             arr[0] = "Kurkre";
-			             arr[1] = "Lays";
-			             arr[2] = "Dairy Milk";
-			             arr[3] = "Oreo";
-			             arr[4] = "Ice Cream";
-			                                           int [] price = {2,2,2,2,2};
-			                
-		System.out.print("ProductID: ");
-		Scanner scanner = new Scanner(System.in);
-			
-		addproduct = scanner.nextInt();
-		System.out.print("Quantity: ");
-		addquantity = scanner.nextInt();
-			 		
-		System.out.println( "Your Procuct: "+arr[(addproduct - 1)]+ " Quantity: "+addquantity+ " added succesfully.");
-		a.add(addquantity);
-		b.add(addproduct);
-		c.add(arr[(addproduct - 1)]);
-		total = total + ( price[(addproduct - 1)] * addquantity);
-		//scanner.close();					
-	}
+			            String products[] = new String[5];
+			            products[0] = "Kurkre\t";
+			            products[1] = "Lays\t";
+			            products[2] = "Dairy Milk";
+			            products[3] = "Oreo\t";
+			            products[4] = "Ice Cream";
+			                                           int [] price = {2,2,2,2,2};			   
+	System.out.print("Product Id: ");
+	Scanner scanner = new Scanner(System.in);
+	addproduct = scanner.nextInt();
+	System.out.print("Quantity: ");
+	addquantity = scanner.nextInt();
+	quantity.add(addquantity);
+	System.out.println("\nYour Procuct: "+products[(addproduct - 1)]+"\n"+"Quantity: "+addquantity+ " added succesfully.\n");
+    Prod product = new Prod(addproduct,addquantity,products[(addproduct - 1)]);
+	productlist.add(product);
+	total = total + ( price[(addproduct - 1)] * addquantity);
+	}	
 	public void show() {
 		
-		if(b.isEmpty()){
+		if(productlist.isEmpty()){
 			System.out.println("Your cart is empty.");
 		}
 		
 		else {
-		    System.out.println("PId\tName\t\tQuantity");
+			Iterator itr=productlist.iterator();
+		    System.out.println("PId\tName\t\t  Quantity");
 		    
-		for(j=1,i=0; i<b.size() && j<=b.size(); i++,j++) {
-			System.out.print( +j  +"\t"+ c.get(i)+"\t\t"+ a.get(i) +"\n");	
+		for(int j=1,i=0; i<productlist.size() && j<=productlist.size(); i++,j++) {
+			Prod st=(Prod)itr.next();
+            System.out.println(+j +"\t"+st.name+"\t\t"+st.quantity);
 		}
-		    System.out.println("\nTotal: "+total);
 		}
+		System.out.println("\nTotal: "+total);
 	}
 	public void remove() {
-		
-		if(b.isEmpty()) {
+		if(productlist.isEmpty()) {
 			System.out.println("Your cart is empty.");
-		}
-		
+		}	
 		else {
 			int r;
 			System.out.print("Enter productId: ");
 			Scanner rem = new Scanner(System.in);
-			r = rem.nextInt();
-			b.remove(r-1);
-			a.remove(r-1);
-			c.remove(r-1);	
-	        t = t + (addquantity * 2);    //(addquantity * price[(addproduct - 1)])
-	 		total = t;
+			r = rem.nextInt();																	
+			productlist.remove(r-1);
+			int qunt = quantity.get(r-1);
+			t =  (qunt * 2);
+		 	total = total - t;
+			quantity.remove(r-1);
 	 		//rem.close();
-		}
+		}		
 	}
 }
+class Prod
+{
+	int productid;
+    int quantity; 
+    String name;
+    Prod(int productid,int quantity, String name){  
+        this.productid=productid;    
+        this.quantity=quantity;
+        this.name=name;
+}
+}
+
 public class Cart {
 	public static void main(String[] args) {
+		
 		int choice=1;
 		int minsequenceno=0;
 		int maxsequenceno=5;
 		Shopping s1 = new Shopping();                                 //Object of Shopping class (s1).
 			System.out.println("\t\tGaurav");
 			System.out.println("\t    Shopping Cart");
-			System.out.println("##########################################\n");
+			System.out.println("##########################################");
 			while(choice>minsequenceno && choice <maxsequenceno) {			
-			System.out.println("Press 1 for list of products.");
+			System.out.println("\nPress 1 for list of products.");
 			System.out.println("Press 2 to add products.");
 			System.out.println("Press 3 to remove products.");
 			System.out.println("Press 4 to view cart.\n");
-			System.out.print("Enter Your choice: ");
+			System.out.print("Enter choice: ");
 			Scanner scan = new Scanner(System.in);
 				choice = scan.nextInt();
 				System.out.println();
@@ -106,7 +112,7 @@ public class Cart {
 				    	 s1.show();
 				    	 break;
 				     }	
-	}
+	}			
 }
 }
 
